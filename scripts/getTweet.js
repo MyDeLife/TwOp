e724851dae7c6856d9e94f939052a8e9b27159b4
Estie <ewoo0886@gmail.com>
2023-03-23 13:57:03 -0500

    update getTweet.js
    added themes object and updated getVoice() to retrieve the corresponding voice of the selected value

--- scripts/getTweet.js
+++ scripts/getTweet.js
@@ -25,17 +25,24 @@ const specificTheme = function () {
     return values.join("")
 }
 
-
+let generalTheme = document.getElementById("general-themes-dropdown").options[document.getElementById("general-themes-dropdown").selectedIndex].value;
 
 const getVoice = function () {
-    if (specificTheme === "") { return '' }
-    for (let key in voices) {
-        if (key === specificTheme()) {
+    if (`${generalTheme}` === "") { return '' }
+    if (`${specificTheme()}` !== "") {
+
+        for (let key in voices) {
+            if (key === `${ specificTheme() }`) {
+                return `8. Write it in the style of ${voices[key]} without mentioning the name in the response.`;
+            }
+        }
+
+    } for (let key in voices) {
+        if (key === `${generalTheme}`) {
             return `8. Write it in the style of ${voices[key]} without mentioning the name in the response.`;
         }
     }
 }
-
 
 
 const generate = async (prompt) => {
@@ -82,8 +89,7 @@ const generateCompletionAction = async () => {
     let keywords = document.getElementById("keywords").value;
 
     // Get user selection from dropdown menus
-    let generalTheme = document.getElementById("general-themes-dropdown").options[document.getElementById("general-themes-dropdown").selectedIndex].value;
-
+    
     let generalStyle = document.getElementById("general-style-dropdown").options[document.getElementById("general-style-dropdown").selectedIndex].value;
     let specificStyle = function () {
         let dropdowns = document.getElementsByClassName("style-deselect");
@@ -108,7 +114,7 @@ const generateCompletionAction = async () => {
         5. Do not use adjectives or verbs in your suggestion more than once.
         6. NEVER include emojis and NEVER include hashtags (i.e. #tweet).
         7. Consider each digit, letter, whitespace, punctuation and symbol as one character. Your response should not exceed a total of 280 characters.
-        ${ getVoice() }`
+        ${getVoice()}`
 
         const firstCompletion = await generate(
             `${firstPrompt}`
@@ -121,7 +127,7 @@ const generateCompletionAction = async () => {
         let output = secondCompletion.choices[0].text.replace(/^\S+\s*/, "").trimStart().replace(/#\S+/g, '');
         document.getElementById("output_text").innerHTML = output;
 
-        
+
     } catch (error) {
 
         console.log(error);
@@ -150,7 +156,95 @@ document.getElementById("button_get_tweet").addEventListener("click", generateCo
 
 // MAP THEME TO OBJECT
 const voices = {
-    School: "Bugs Bunny",
-    weather: "Al Roker",
-    war2: "Jeff Corwin"
+    "Business & Finance": "Jack Welch", //general theme
+    Cryptocurrency: "Michael Novogratz",
+    Economics: "Milton Friedman",
+    "Financial Planning": "Suze Orman",
+    Investing: "Warren Buffet",
+    Leadership: "Steve Jobs",
+    "Merger & Acquisitions": "Jack Welch",
+    "Research & Development": "Elon Musk",
+    Sales: "Zig Ziglar",
+    "Start-Ups": "Jack Welch",
+    "Stock Market": "Jim Cramer",
+    Taxation: "Thomas Jefferson",
+    Trading: "Jim Cramer",
+    Ecosystem: "Morgan Freeman", //general theme
+    Climate: "Al Roker",
+    Demonstration: "Martin Luther King Jr",
+    Energy: "Captain Planet",
+    Law: "Ruth Bader Ginsburg",
+    Nature: "James Earl Jones",
+    News: "Bill Maher",
+    Politics: "Margaret Thatcher",
+    "Real Estate": "Donald Trump",
+    War: "George Washington",
+    Wildlife: "Jeff Corwin",
+    Entertainment: "Ellen DeGeneres", //general theme
+    Celebrities: "Billy Crystal",
+    Dancing: "Austin Powers",
+    Games: "Robin Williams",
+    Movies: "Angelina Jolie",
+    Music: "Beyonce",
+    Documentaries: "David Attenborough",
+    "Game Shows": "Terry Crews",
+    "Reality Shows": "Simon Cowell",
+    "TV Series": "Tina Fey",
+    Leisure: "Martha Stewart", //general theme
+    Arts: "Andy Warhol",
+    Bars: "Jack Sparrow",
+    Books: "Stephen King",
+    Cooking: "Rachel Ray",
+    Fashion: "Donatella Versace",
+    Firearms: "John Wayne",
+    Fitness: "Arnold Schwarzenegger",
+    Gambling: "Daniel Negreanu",
+    Hobbies: "Emma Watson",
+    Photography: "Annie Leibovitz",
+    Restaurants: "Gordon Ramsay",
+    Shopping: "Kim Kardashian",
+    "Social Networking": "Peter Griffin (from Family Guy)",
+    Travel: "Ernest Hemingway",
+    Life: "Nigella Lawson", //general theme
+    Career: "Sheryl Sandberg",
+    Cats: "Catwoman",
+    Dating: "Marilyn Monroe",
+    Dogs: "Snoopy",
+    Education: "Michael Fullan",
+    Family: "Dr. Phil",
+    Gardening: "Tovah Martin",
+    Health: "Dr. Sanjay Gupta",
+    Holidays: "Mark Twain",
+    "Home Decorating": "Christina Anstead",
+    Parenting: "Dr. Seuss",
+    Religion: "Mother Teresa",
+    "Science & Tech": "Elon Musk", //general theme
+    Aerospace: "Captain Kirk",
+    "Artificial Intelligence": "HAL 9000",
+    "Augmented Reality": "Optimus Prime",
+    "Autonomous Systems": "Data (from Star Trek)",
+    "Biomedical Engineering": "Data (from Star Trek)",
+    Cybersecurity: "Data (from Star Trek)",
+    "Quantum Computing": "Sheldon Cooper",
+    Robotics: "C3PO",
+    Web3: "Vitalik Buterin",
+    "Sports": "Serena Williams", //general theme
+    "American Football": "Tom Brady",
+    Athletics: "Usain Bolt",
+    Baseball: "Babe Ruth",
+    Boxing: "Muhammad Ali",
+    Cricket: "Sachin Tendulkar",
+    "Extreme Sports": "Shaun White",
+    "Football (Soccer)": "Gary Lineker",
+    Golf: "Arnold Palmer",
+    "Ice Hockey": "Wayne Gretzky",
+    "Martial Arts": "Chuck Norris",
+    Motorsport: "Lewis Hamilton",
+    Rugby: "Martin Johnson",
+    Surfing: "Stephanie Gilmore",
+    Tennis: "John McEnroe",
+    "Winter Sports": "Lindsey Vonn",
+    Wrestling: "Hulk Hogan",
+
 };
+
